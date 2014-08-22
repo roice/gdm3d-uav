31a4710b19cd6acfa4ba2e3b044c4116f203c939
roice <xingqing_lhtv@qq.com>
2014-08-22 09:49:36 +0800

    WIP

--- src/mavlink_udp.cpp
+++ src/mavlink_udp.cpp
@@ -2,6 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>  /* UNIX standard function definitions */
+#include <fcntl.h>   /* File control definitions */
+#include <errno.h>   /* Error number definitions */
+/* Network includes */
+#include <sys/socket.h> /* socket functions and data structures */
+#include <sys/types.h>  /* basic system data types */
+/* Linux POSIX timer headers */
+#include <sys/time.h>   /* headers for date and time of linux system */
+#include <time.h>       /* headers for date and time */
+#include <arpa/inet.h>  /* provide conversion function of IP address */
 /* pThread and sem related includes */
 #include <pthread.h>
 #include <semaphore.h>
@@ -15,6 +25,8 @@
 extern mavlink_message_t message_mavlink_uart_received;
 // Semaphores
 extern sem_t sem_mavlink_serial_message_received;
+// micro seconds since epoch
+uint64_t microsSinceEpoch();
 
 void *mavlink_udp_send_thread_func(void *arg)
 {
